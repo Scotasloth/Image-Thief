fac9c2f6e26faa893af755c9041c46b12d9b305b
Scotasloth <rossmcc76@gmail.com>
2024-10-25 21:59:22 +0100

    Attempting to display all found images on a localhost server

--- Image Thief.js
+++ Image Thief.js
@@ -2,6 +2,9 @@ const axios = require('axios'); // Importing axios for making HTTP requests
 const cheerio = require('cheerio'); // Importing cheerio for parsing HTML
 const express = require('express');
 
+const app = express();
+const PORT = 3000;
+
 async function getImages(url) {
     try{
         const { data } = await axios.get(url);
@@ -24,6 +27,16 @@ async function getImages(url) {
     }
 }
 
-getImages("https://Google.co.uk").then(images => {
-    console.log("Images found: ", images);  
+app.get("/images", async (req, res) => {
+    const images = await getImages("https://Google.co.uk")
+    res.send(`
+        <h1>Images Found</h1>
+        <div>
+            ${images.map(img => `<img src="${img}" style="width: 200px; margin: 10px;" />`).join('')}
+        </div>
+    `); 
 });
+
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:${PORT}")
+})
