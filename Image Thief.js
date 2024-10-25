c4526ca2787e91e184fd1d4829cab887cea11c76
Scotasloth <rossmcc76@gmail.com>
2024-10-25 21:38:35 +0100

    Wrote some code for the scraping function

--- Image Thief.js
+++ Image Thief.js
@@ -1,3 +1,21 @@
 const axios = require('axios')
 const cheerio = require('cherrio')
 const express = require('express')
+
+async function getImages(url) {
+    try{
+        const { data } = await axios.get(url);
+
+        const $ = cheerio.load(data);
+
+        const images = [];
+
+    } catch (error) {
+        console.error("Error scraping images: ", error);
+        return [];
+    }
+}
+
+getImages("https://Google.co.uk").then(images => {
+    console.log("Images found: ", images);  
+});
